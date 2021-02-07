8576a3c0e14f2631723fac5e44d1aa7dd66980a3
Andreas Evensen <andreas.evensen11@gmail.com>
2021-02-07 18:36:23 +0100

    Create handin2.py

---
+++ Handin/handin2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+mg = 10
+
+xlist = np.linspace(0,np.pi/2,100)
+f1 = 3*mg/2*(np.sin(xlist)*np.cos(xlist)*3/2-np.cos(xlist))
+f2 = 3*mg/2*(-np.sin(xlist)+(3*np.sin(xlist)**2-1)/2) + mg
+
+plt.plot(xlist, f1, '-', markersize=1, label = r"$F_x$")
+plt.plot(xlist, f2, '-', markersize=1, label = r"$F_y$")
+plt.grid()
+plt.legend()
+plt.show()
