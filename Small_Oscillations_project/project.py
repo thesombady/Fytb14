e630e8d9ff4512849d124426b5754b6b44d89069
Andreas Evensen <andreas.evensen11@gmail.com>
2021-03-10 11:13:20 +0100

    Update project.py

--- Small_Oscillations_project/project.py
+++ Small_Oscillations_project/project.py
@@ -7,14 +7,10 @@ m1 = Symbol('m1', postive = True)
 m2 = Symbol('m2', postive = True)
 m3 = Symbol('m3', postive = True)
 m = Symbol('m', postive = True)
-import SacraMathEngine as s
 
-M=Matrix([[m1,0,0,0,0,0],[0,m2,0,0,0,0], [0,0,m3,0,0,0],[0,0,0,m1,0,0],[0,0,0,0,m2,0],[0,0,0,0,0,m3]])
 k = Symbol('k')
 w = Symbol('w')
-
-M_m = Matrix([[m,0,0,0,0,0],[0,m,0,0,0,0], [0,0,m,0,0,0],[0,0,0,m,0,0],[0,0,0,0,m,0],[0,0,0,0,0,m]])
-
+M_m = m*eye(6)
 q = np.sqrt(3)/4
 K = k*Matrix([[5/4,-1,-1/4,q,0,-q],
     [-1,5/4, -1/4,0,-q, q],
@@ -29,18 +25,7 @@ K2 = k*Matrix([[5/4, -1, -1/4, q, 0, -q],
     [0,-q, q, 0, 3/4, -3/4],
     [-q, q, 0, -3/4,-3/4, 3/2]])
 
-
-#print((K-w**2*M_m).eigenvals())
-#print(K2.shape)
 eigenvectors = (K2-w**2*M_m).eigenvects()
-#print((K2-w**2*M_m).det())
-"""
-for i in range(len(eigenvectors)):
-    print("Eigenvalue:", eigenvectors[i][0])
-    print("multiplicity:", eigenvectors[i][1])
-    print("Eigenvectors:", eigenvectors[i][2])
-"""
-
 for i in range(len(eigenvectors)):
     try:
         print("new vector")
